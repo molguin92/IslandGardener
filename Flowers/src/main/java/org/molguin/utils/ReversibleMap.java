b27a82111574db61aaf6e73eb3a5b5fedb73d0bb
Manuel Olguín Muñoz <manuel@olguin.se>
2020-04-30 00:38:47 -0400

    implement more map operations

--- Flowers/src/main/java/org/molguin/utils/ReversibleMap.java
+++ Flowers/src/main/java/org/molguin/utils/ReversibleMap.java
@@ -10,13 +10,14 @@ import java.util.concurrent.ConcurrentSkipListSet;
 public class ReversibleMap<K extends Comparable, V extends Comparable> extends ConcurrentHashMap<K, V> {
     private final ConcurrentMap<V, Set<K>> revmap = new ConcurrentHashMap<>();
 
+    public Set<K> getKeysForValue(V v) {
+        return new HashSet<>(this.revmap.get(v));
+    }
+
     @Override
     public V put(K k, V v) {
         V result = super.put(k, v);
-        Set<K> kset = this.revmap.get(v);
-        if (kset == null) kset = new ConcurrentSkipListSet<>();
-        kset.add(k);
-        this.revmap.put(v, kset);
+        this.putIntoRevMap(k, v);
         return result;
     }
 
@@ -33,7 +34,54 @@ public class ReversibleMap<K extends Comparable, V extends Comparable> extends C
         else return null;
     }
 
-    public Set<K> getKeysForValue(V v) {
-        return new HashSet<>(this.revmap.get(v));
+    private void removeFromRevMap(K k, V v) {
+        Set<K> revResult = this.revmap.get(v);
+        revResult.remove(k);
+        if (revResult.isEmpty()) this.revmap.remove(v);
+        else this.revmap.put(v, revResult);
+    }
+
+    private void putIntoRevMap(K k, V v) {
+        Set<K> kset = this.revmap.get(v);
+        if (kset == null) kset = new ConcurrentSkipListSet<>();
+        kset.add(k);
+        this.revmap.put(v, kset);
+    }
+
+
+    @Override
+    public V remove(Object k) {
+        V result = super.remove(k);
+        if (result != null)
+            this.removeFromRevMap((K) k, result);
+        return result;
+    }
+
+    @Override
+    public boolean remove(Object k, Object v) {
+        boolean result = super.remove(k, v);
+        if (result)
+            this.removeFromRevMap((K) k, (V) v);
+        return result;
+    }
+
+    @Override
+    public V replace(K k, V v) {
+        V old = super.replace(k, v);
+        if (old != null) {
+            this.removeFromRevMap(k, old);
+            this.putIntoRevMap(k, v);
+        }
+        return old;
+    }
+
+    @Override
+    public boolean replace(K k, V o, V n) {
+        boolean replaced = super.replace(k, o, n);
+        if (replaced) {
+            this.removeFromRevMap(k, o);
+            this.putIntoRevMap(k, n);
+        }
+        return replaced;
     }
 }
